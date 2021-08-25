c67632938abb66a82efd47d319d33adc44130cb8
LauNM <laurie.beduneau@gmail.com>
2021-08-25 17:43:46 +0200

    finished validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,6 +9,7 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalBody = document.querySelector(".modal-body");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelector(".close");
@@ -29,75 +30,109 @@ function closeModal() {
     modalbg.style.display = "none";
 }
 
-/*const form = document.getElementById('form');
-const firstName = document.getElementById('first');
-const lastName = document.getElementById('last');
-
-document.addEventListener("DOMContentLoaded", function () {
-    let elements = document.getElementsByTagName("INPUT");
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].oninvalid = function (e) {
-            e.target.setCustomValidity("");
-            if (!e.target.validity.valid) {
-                e.target.setCustomValidity("This field cannot be left blank");
-            }
-        };
-        elements[i].oninput = function (e) {
-            e.target.setCustomValidity("");
-        };
-    }
-})*/
+//REGEX CONSTANTS
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const numberRegex = /^[0-9]*$/;
 
 function submitForm() {
+
     event.preventDefault();
-    const form = document.getElementById('form');
-    const formData = document.getElementById('formData');
+    const form = document.getElementById('reserve');
     const firstName = document.getElementById('first');
     const lastName = document.getElementById('last');
     const email = document.getElementById('email');
     const birthdayInput = document.getElementById('birthdate');
+    const today = new Date();
+    const birthdate = new Date(birthdayInput.value);
     const quantity = document.getElementById('quantity');
+    const locationCheckboxes = document.querySelectorAll('input[name="location"]');
+    const checkedOne = Array.prototype.slice.call(locationCheckboxes).some(x => x.checked);
+    const utilisationConditions = document.getElementById('checkbox1');
 
-    let emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    let numberRegex = /^[0-9]*$/;
-
-    if (firstName.value.length < 2 || lastName.value.length < 2) {
-        firstName.setAttribute('data-error', 'Ce champ doit contenir au moins 2 caractères');
-        alert("nop");
-        return false;
-    }
-    if (email.value === "" || emailRegex.test(email.value) === false) {
-        alert("Cette adresse mail n'est pas valide");
-        return false;
+    //Set data-error-visible to false everytime the form is submitted
+    for (let i = 0; i < formData.length; i++) {
+        formData[i].setAttribute('data-error-visible', 'false')
     }
 
-    //Compare with today's date
-    let dateOfToday = new Date();
-    let month = dateOfToday.getMonth();
-    let day = dateOfToday.getDate();
-    let year = dateOfToday.getFullYear();
+    let messageValidity;
+    function checkValidity() {
 
-    console.log(birthdayInput.value)
-    let today = new Date(year, month, day);
-    let birthdate = new Date(birthdayInput.value);
-    if (birthdate > today) {
-        alert("A moins d'être un voyageur du temps, vous avez dû vous tromper de date");
-        return false;
-    }
-    if (birthdayInput.value === "") {
-        alert("Veuillez renseigner votre date de naissance");
-        return false;
-    }
-    if (quantity.value === "") {
-        alert("Vous devez rentrer une valeur");
-        return false;
-    }
+        //ForEach input, check if the conditions are respected
+        if (firstName.value.length < 2) {
+            firstName.parentElement.setAttribute('data-error', 'Ce champ doit contenir au moins 2 caractères');
+            firstName.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+        if (lastName.value.length < 2) {
+            lastName.parentElement.setAttribute('data-error', 'Ce champ doit contenir au moins 2 caractères');
+            lastName.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+        // Check if email is not null and if it's actually an email address
+        if (email.value.length < 1 || emailRegex.test(email.value) === false) {
+            email.parentElement.setAttribute('data-error', 'Cette adresse mail n\'est pas valide');
+            email.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
 
-    if (numberRegex.test(quantity.value) === false) {
-        alert("Ceci doit être une valeur numérique !");
+        //Compare birthdate with today's date
+
+        if (birthdate > today) {
+            birthdayInput.parentElement.setAttribute('data-error', 'Il n\'est pas possible de donner une date dans le futur');
+            birthdayInput.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+        if (birthdayInput.value.length < 1) {
+            birthdayInput.parentElement.setAttribute('data-error', 'Veuillez renseigner votre date de naissance');
+            birthdayInput.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+        if (quantity.value.length < 1) {
+            quantity.parentElement.setAttribute('data-error', 'Veuillez renseigner un nombre de tournois');
+            quantity.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+
+        //Check if the value is a number and if it's > or === to 0
+        if (numberRegex.test(quantity.value) === false || quantity.value < 0) {
+            quantity.parentElement.setAttribute('data-error', 'Veuillez renseigner une valeur numérique supérieure ou égale à 0');
+            quantity.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+
+
+        //Check if at least 1 city is checked
+        if (checkedOne === false) {
+            locationCheckboxes[0].parentElement.setAttribute('data-error', 'Veuillez sélectionner au moins une ville');
+            locationCheckboxes[0].parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+
+        //Check if the checkbox is checked
+        if (utilisationConditions.checked === false) {
+            utilisationConditions.parentElement.setAttribute('data-error', 'Vous devez accepter les conditions d\'utilisation pour valider votre inscription');
+            utilisationConditions.parentElement.setAttribute('data-error-visible', 'true');
+            messageValidity = 'error';
+        }
+
+        else {
+            messageValidity = 'formValid';
+        }
+        return messageValidity;
+    }
+    checkValidity();
+    if (messageValidity === 'formValid') {
+        form.reset();
+        form.style.display="none";
+        modalBody.innerHTML="Merci ! Votre réservation a été reçue.";
+        let btnClose = document.createElement("button");
+        btnClose.innerHTML = "Fermer";
+        btnClose.className = "button btn-submit";
+        btnClose.setAttribute("onClick", "closeModal()");
+        modalBody.appendChild(btnClose);
+    }
+    else {
         return false;
     }
-
 }
 
